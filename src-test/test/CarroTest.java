71c60e7413a7db81675a1a7157bed087dfe638a4
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2019-07-25 20:44:00 -0300

    Inserindo delegate e classe teste

---
+++ src-test/test/CarroTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public class CarroTest extends TestCase {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/br/com/pessoa/domain/CarroService.java
@@ -0,0 +1,65 @@
+package br.com.pessoa.domain;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Classe intermediaria entre a camada web e a camada de persistencia
+ * 
+*/
+public class CarroService {
+	private CarroDAO db = new CarroDAO();
+	
+	public List<Carro> getCarros() throws Exception{
+		try {
+			List<Carro> carros = this.db.getCarros();
+			return carros;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+	}
+	
+	public List<Carro> findByName(String nome) throws Exception{
+		try {
+			List<Carro> carros = this.db.findByName(nome);
+			return carros;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+	}
+	
+	public List<Carro> findByTipo(String tipo) throws Exception{
+		try {
+			List<Carro> carros = this.db.findByTipo(tipo);
+			return carros;
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+	}
+	
+	public boolean delete(Long id) throws Exception{
+		try {
+			return this.db.deleteCarro(id);			
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+	}
+	
+	public boolean save(Carro carro) throws Exception{
+		try {
+			this.db.save(carro);
+			return true;
+		} catch (Exception e) {
+			return false; 
+		}
+	}
+	
+	public Carro getCarro(Long id) throws Exception{
+		try {
+			return this.db.getCarroById(id);			
+		} catch (Exception e) {
+			throw new Exception(e.getMessage()); 
+		}
+	}
+
+}
